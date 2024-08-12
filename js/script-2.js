67b1db2f926b3a3d18cbcba390f9d1ae715501a2
Владислав <arshavin0309@mail.ru>
2024-08-12 16:21:39 +0500

    add

--- js/script-2.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let headerBurger = document.querySelector('.header__burger');
-    let headerMenu = document.querySelector('header .menu');
-
-    headerBurger.addEventListener('click', () => {
-        headerMenu.classList.toggle('active');
-    });
-});
