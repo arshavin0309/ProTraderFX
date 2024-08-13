f787983a52a4c7b2225b2b3b85c71ba8bcdd038f
Владислав <arshavin0309@mail.ru>
2024-08-13 14:44:17 +0500

    add

--- js/script.js
+++ js/script.js
@@ -108,6 +108,21 @@ document.addEventListener('DOMContentLoaded', () => {
         pagination: {
             el: '.swiper-pagination',
         },
+
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+            650: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            1000: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            }
+        }
     });
 
     let responsibilityBtn = document.querySelectorAll('.responsibility__btn');
